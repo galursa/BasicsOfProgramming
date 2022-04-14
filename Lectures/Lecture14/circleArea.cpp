0f045adc888b66dc63608203061a3e91eb653c02
galursa <jakula@poczta.fm>
2022-04-14 15:04:19 +0200

    Lecture 14 examples

---
+++ Lectures/Lecture14/circleArea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define pi 3.14
+
+using namespace std;
+int main(int argc, char** argv) {
+	cout<<"Calculating circle area. Write radius \n";
+	float radius;
+	cin>>radius;
+	cout<<"Circle are is:"<<pi*radius*radius<<"\n";
+	return 0;
+}
